ac1f2c26d45fd099e7e824e279e1a354805aa15e
Yannick MOHL-CLAUZADE <ymohl-cl@e2r9p1.42.fr>
2015-09-22 17:54:14 +0200

    add generate package

--- board/board.go
+++ board/board.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/Wouterbeets/n-puzzle/plog"
 	"github.com/Wouterbeets/n-puzzle/solver"
+	"math/rand"
 	"strconv"
 	"strings"
 )
@@ -13,6 +14,7 @@ const (
 	Down
 	Left
 	Right
+	MAX_SIZE = 42
 )
 
 type tile struct {
@@ -260,4 +262,56 @@ func init() {
 	moveMap[Down] = (*Board).moveDown
 	moveMap[Left] = (*Board).moveLeft
 	moveMap[Right] = (*Board).moveRight
+}
+
+// Return a new slice with all map's values
+// sval = slice_value
+func Get_slice(size int) (sval []int) {
+	size = size * size
+	for i := 0; i < size; i++ {
+		sval = append(sval, i)
+	}
+	return sval
+}
+
+func Delete_elem(g_slice []int, index int) (n_slice []int) {
+	size := len(g_slice) - 1
+	for i := 0; i < size; i++ {
+		if i != index {
+			n_slice = append(n_slice, g_slice[i])
+		}
+	}
+	return n_slice
+}
+
+func Get_value(g_slice []int) ([]int, int) {
+	size := len(g_slice)
+	r := rand.New(rand.NewSource(int64(size)))
+	index := r.Int()
+	if index == 0 {
+		index = 1
+	}
+	value := g_slice[index]
+	g_slice = Delete_elem(g_slice, index)
+	return g_slice, value
+}
+
+func GetMap() (*Board, error) {
+	r := rand.New(rand.NewSource(MAX_SIZE))
+	size := r.Int()
+	if size < 3 {
+		size = 3
+	}
+	b := New(size)
+	sval := Get_slice(size)
+	for i := 0; i < b.size; i++ {
+		for j := 0; j < b.size; j++ {
+			sval, b.Rows[i][j].val = Get_value(sval)
+			if b.Rows[i][j].val == 0 {
+				b.BR = i
+				b.BC = j
+			}
+		}
+	}
+	return b, nil
 }
