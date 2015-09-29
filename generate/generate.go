6a90034e2cd6596cd0b4b29738f801bbe4f45ccc
Yannick MOHL-CLAUZADE <ymohl-cl@e2r10p6.42.fr>
2015-09-29 16:01:49 +0200

    generate ok

--- generate/generate.go
+++ generate/generate.go
@@ -1,20 +1,17 @@
 package generate
 
 import (
-	"fmt"
 	"github.com/Wouterbeets/n-puzzle/board"
 	"math/rand"
 )
 
 const (
-	MAX_SIZE = 4
+	MAX_SIZE = 3
 )
 
 // Return a new slice with all map's Values
 // sVal = slice_Value
 func Get_slice(size int) (sVal []int) {
-	fmt.Println("Dans Get_slice:")
-	fmt.Println(size)
 	size = size * size
 	for i := 0; i < size; i++ {
 		sVal = append(sVal, i)
@@ -24,7 +21,7 @@ func Get_slice(size int) (sVal []int) {
 
 // dVal = delete_Value
 func Delete_elem(g_slice []int, index int) (n_slice []int) {
-	size := len(g_slice) - 1
+	size := len(g_slice)
 	for i := 0; i < size; i++ {
 		if i != index {
 			n_slice = append(n_slice, g_slice[i])
@@ -36,22 +33,13 @@ func Delete_elem(g_slice []int, index int) (n_slice []int) {
 func Get_Value(g_slice []int) ([]int, int) {
 	size := len(g_slice)
 	index := rand.Intn(size)
-	fmt.Println("value de index")
-	fmt.Println(index)
-	fmt.Println("value de size")
-	fmt.Println(size)
-	if index == 0 {
-		index = 1
-	}
 	Value := g_slice[index]
 	g_slice = Delete_elem(g_slice, index)
 	return g_slice, Value
 }
 
 func GetMap() (*board.Board, error) {
-	size := rand.Intn(MAX_SIZE)
-	fmt.Println("In Generate, size:")
-	fmt.Println(size)
+	size := rand.Intn(MAX_SIZE + 1)
 	if size < 3 {
 		size = 3
 	}
